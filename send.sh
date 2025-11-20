3ac68b30f4b7af3bfd99d4d7ac4517d6fb217e1e
rotarymars <s.goto2050@gmail.com>
2025-11-20 16:14:34 +0900

    modified which file to send

--- send.sh
+++ send.sh
@@ -1,3 +1,3 @@
-find . -name "*.cpp" -o -name "*.h" -o -name "*.c" -o -name "*.hpp" | while read file; do
+find . -name "*.cpp" -o -name "*.h" -o -name "*.c" -o -name "*.hpp -o -name "*.ini" | while read file; do
     rsync -av --mkpath --delete --exclude .direnv/ --exclude .git/ "$file" robo@roboberry.local:robocup2026-esp32-kanto/$file
 done
