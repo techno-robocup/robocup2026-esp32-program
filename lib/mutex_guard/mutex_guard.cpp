f7a5eac2204493dc0d976fbe43eb06f9fab8bec4
K10-K10 <keito.tottori@gmail.com>
2025-11-20 16:11:21 +0900

    format

--- lib/mutex_guard/mutex_guard.cpp
+++ lib/mutex_guard/mutex_guard.cpp
@@ -1,5 +1,7 @@
 #include "mutex_guard.hpp"
 
-MutexGuard::MutexGuard(SemaphoreHandle_t sem) : sem(sem) { xSemaphoreTake(sem, portMAX_DELAY); }
+MutexGuard::MutexGuard(SemaphoreHandle_t sem) : sem(sem) {
+  xSemaphoreTake(sem, portMAX_DELAY);
+}
 
 MutexGuard::~MutexGuard() { xSemaphoreGive(sem); }
