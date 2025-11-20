5d83ca37b2ff9867341cde7d27e8268e9f3a855c
rotarymars <s.goto2050@gmail.com>
2025-11-20 16:10:04 +0900

    adding mkpath in send.sh The script used to fail when there wasn't a parent directory when sending codes. By this command, it is changed to create parent directory if there wasn't.

--- send.sh
+++ send.sh
@@ -1,3 +1,3 @@
 find . -name "*.cpp" -o -name "*.h" -o -name "*.c" -o -name "*.hpp" | while read file; do
-    rsync -av --delete --exclude .direnv/ --exclude .git/ "$file" robo@roboberry.local:robocup2026-esp32-kanto/$file
+    rsync -av --mkpath --delete --exclude .direnv/ --exclude .git/ "$file" robo@roboberry.local:robocup2026-esp32-kanto/$file
 done
