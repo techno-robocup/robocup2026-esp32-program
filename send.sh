9f8eeed8f268eab8cf372fa34078962d7e909204
rotarymars <s.goto2050@gmail.com>
2026-02-13 16:19:14 +0900

    fixing send script
    It used to send the dependencies' files, but that's not a desired
    behaviour, so it is fixed.

--- send.sh
+++ send.sh
@@ -1,3 +1,3 @@
-find . -name "*.cpp" -o -name "*.h" -o -name "*.c" -o -name "*.hpp" -o -name "*.ini" | while read file; do
-    rsync -av --mkpath --delete --exclude .direnv/ --exclude .git/ "$file" robo@roboberry.local:robocup2026-esp32-program/$file
+find src lib include -type f \( -name "*.cpp" -o -name "*.h" -o -name "*.c" -o -name "*.hpp" \) | while IFS= read -r file; do
+  rsync -av --mkpath --delete --exclude .direnv/ --exclude .git/ "$file" robo@roboberry.local:robocup2026-esp32-program/$file
 done
